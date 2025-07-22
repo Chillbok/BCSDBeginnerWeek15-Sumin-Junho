a8dd82a4bbdfac810edd5315371d6f4f128ad581
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-07-22 22:56:30 +0900

    feat(ui): 버프 분, 초 계산해서 출력하도록 하는 메서드 생성
    버프 시간은 float, 버프 이름은 string으로 설정했다는 가정 하에 코드를 작성함.
    버프 이름, 지속시간을 동시에 표시하는 string 값을 출력하는 메서드를 만드는 것까지는 완료. 다만 이후의 작업은 플레이어에게 적용되는 버프를 구현하고 나서 해야 할 것 같음.
    - 한 번에 버프가 여러개 활성화된 경우 몇 개까지 표시할 것인가
    - 버프를 어떻게 구현할 것인가?

--- Assets/Scripts/UI/BuffUIController.cs
+++ Assets/Scripts/UI/BuffUIController.cs
@@ -9,8 +9,8 @@ public class BuffUIController : MonoBehaviour
     //참조 변수(버프 이름 및 지속시간 가져올 참조변수)
 
     //가져올 변수
-    private float buffDuration; //버프 지속시간
     private string buffName; //버프 이름
+    private float buffDuration; //버프 지속시간
 
     void Start()
     {
@@ -18,5 +18,20 @@ public class BuffUIController : MonoBehaviour
 
     void Update()
     {
+    }
+
+    //입력한 버프 시간, 버프 이름을 반환 --> 버프이름 버프시간(분):버프시간(초)
+    public string writeBuffText(string buffName, float buffDuration)
+    {
+        int minutes = Mathf.FloorToInt(buffDuration / 60); //버프 시간 분
+        int seconds = Mathf.FloorToInt(buffDuration % 60); //버프 시간 초
+
+        if (minutes != 0 && seconds != 0)
+        {
+            string buffStatus = $"{buffName} {minutes}:{seconds}";
+            return buffStatus;
+        }
+        else
+            return "";
     }
 }
