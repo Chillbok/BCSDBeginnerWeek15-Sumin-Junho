ccd817aa7dd51b6b61049ff710431fd4fffe326b
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-07-22 20:53:56 +0900

    feat(ui): 버프 UI를 출력하기 위한 참조 변수 2개 추가

--- Assets/Scripts/UI/BuffUIController.cs
+++ Assets/Scripts/UI/BuffUIController.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BuffUIController : MonoBehaviour
 {
+
+    //참조 변수(버프 이름 및 지속시간 가져올 참조변수)
+
+    //가져올 변수
+    private float buffDuration; //버프 지속시간
+    private string buffName; //버프 이름
+
     void Start()
     {
     }
