30a91eae9996f1cc030f59a01453eb89e4a3f3ca
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-07-27 20:44:59 +0900

    feat(ScoreUIController.cs): 스코어를 UI에 출력하기 위한 스크립트 추가

---
+++ Assets/Scripts/UI/ScoreUIController.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreUIController : MonoBehaviour
+{
+    TextMeshProUGUI playTimeText;
+    TextMeshProUGUI maxScoreText;
+    TextMeshProUGUI currentScoreText;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
