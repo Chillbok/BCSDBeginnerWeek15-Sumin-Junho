6b7a5e276b26b00342833d62ea1558d713addabd
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-08-03 03:00:07 +0900

    feat(ui): 게임 스타트 버튼 구현 완료
    게임 스타트 버튼을 누를 시 GamePlayScene이 로드됨.
    이 씬을 불러오는 메서드는 이후 다른 버튼에서도 다른 씬을 불러오기 위해 사용될 여지가 있다는 점을 염두에 두어야 함

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -1,6 +1,7 @@
 //버튼의 기능을 구현한 스크립트
 //모든 버튼의 기능을 이곳에 서술함
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
@@ -12,5 +13,11 @@ public class ButtonController : MonoBehaviour
 #else //유니티 에디터가 아니라면
         Application.Quit();
 #endif
+    }
+
+    //특정 씬으로 이동하는 스크립트
+    public void MoveToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
