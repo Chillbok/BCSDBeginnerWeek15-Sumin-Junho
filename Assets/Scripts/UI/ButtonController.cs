7422c5dcbbc93c484c1d5352c4971dcb553db3b1
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-08-03 00:35:55 +0900

    feat(ui): 게임 종료 버튼 구현
    게임 종료버튼을 구현함.
    ButtonController 스크립트를 구현했으며, 앞으로 버튼 기능을 구현하기 위한 메서드는 해당 스크립트에서 구현할 예정.
    에디터에서는 게임 종료를 구현할 수 없기 때문에, 에디터에서 게임 종료 버튼을 누를 시 에디터 플레이 모드가 종료됨.

---
+++ Assets/Scripts/UI/ButtonController.cs
@@ -0,0 +1,16 @@
+//버튼의 기능을 구현한 스크립트
+//모든 버튼의 기능을 이곳에 서술함
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    //게임 종료시키는 스크립트
+    public void QuitGame()
+    {
+#if UNITY_EDITOR //유니티 에디터라면
+        UnityEditor.EditorApplication.isPlaying = false;
+#else //유니티 에디터가 아니라면
+        Application.Quit();
+#endif
+    }
+}
