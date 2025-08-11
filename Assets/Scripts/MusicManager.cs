6d32034d938b59067ea432a2214d77b9005c473a
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-08-11 19:30:51 +0900

    fix(issue#33): 효과음이 들리지 않는 문제 해결
    GamePlayScene에 있는 SoundManager를 타이틀에 복사했음.
    기존 SoundManager의 BGM 재생 부분은 MusicManager라는 새로운 게임 오브젝트와 스크립트를 만들어 해결함.
    다만 새로운 문제가 생김: 게임 플레이 단계에서 몇번 반복 재생을 하다 갑자기 타이틀 스크린 배경음악을 재생하는 문제가 발생함.
    원인을 모르겠음. 새로운 이슈 생성 예정.
    
    close: issue#33
    
    close: issue#33

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,11 +1,13 @@
 //노래의 출력을 관리함
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(AudioSource))]
 public class MusicManager : Singleton<MusicManager>
 {
+    [SerializeField]
     private AudioSource audioSource;
 
     [Header("음악 목록")]
@@ -14,63 +16,79 @@ public class MusicManager : Singleton<MusicManager>
     public AudioClip[] gamePlayMusic;
     public AudioClip[] gameResultMusic;
 
-    protected override void Awake()
+    //씬 로드 완료 시
+    void OnEnable()
     {
-        audioSource = GetComponent<AudioSource>();
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    //오브젝트 비활성화 시
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //씬 로드 완료되면 호출
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        switch (scene.name)
+        ChooseBGM(scene.name);
+    }
+
+    // BGM 재생(씬이 로드될 때마다 호출)
+    void ChooseBGM(string sceneName)
+    {
+        switch (sceneName)
         {
             //case "GameStartScene":
             case "GameStartScene_withMusic":
                 if (gameStartMusic != null)
                 {
-                    if (gameStartMusic.Length >= 2)
-                        StartCoroutine(PlayIntroLoopMusic(gameStartMusic[0], gameStartMusic[1]));
-                    else if (gameStartMusic.Length > 0)
-                        PlayMusic(gameStartMusic[0]);
+                    if (gameStartMusic.Length >= 2) StartCoroutine(PlayIntroLoopMusic(gameStartMusic[0], gameStartMusic[1]));
                 }
                 break;
             case "GamePlayScene":
+                if (gameStartMusic != null)
+                {
+                    if (gamePlayMusic != null) PlayLoopMusic(gamePlayMusic[0]);
+                }
                 break;
             case "GameResultScene":
+                audioSource.Stop();
                 break;
         }
     }
 
-    //시작부분이 있는 반복 음악
-    IEnumerator PlayIntroLoopMusic(AudioClip intro, AudioClip loop)
+    //시작부분 한번 연주하고, 계속 반복해서 뒷부분 연주
+    IEnumerator PlayIntroLoopMusic(AudioClip introMusic, AudioClip loopMusic)
     {
+        audioSource.Stop();
         //시작부분 한번만 연주
-        audioSource.clip = intro;
+        Debug.Log("시작 부분 연주 시작");
+        audioSource.clip = introMusic;
         audioSource.loop = false;
         audioSource.Play();
 
         //시작부분 음악만큼 기다리기
-        yield return new WaitForSeconds(intro.length);
+        yield return new WaitForSeconds(introMusic.length - 1.2f);
 
         //반복 연주 부분 계속해서 연주하기
-        audioSource.clip = loop;
+        Debug.Log("반복 부분 연주 시작");
+        audioSource.clip = loopMusic;
         audioSource.loop = true;
         audioSource.Play();
     }
 
-    //시작 부분 없이 반복 음악
-    void PlayMusic(AudioClip clip)
+    //시작부분 없이 하나만 반복해서 연주하기
+    void PlayLoopMusic(AudioClip clip)
     {
+        Debug.Log("반복음악 연주!");
+        audioSource.Stop();
         if (clip != null)
         {
             audioSource.clip = clip;
             audioSource.loop = true;
             audioSource.Play();
         }
-    }
-
-    void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
+        else Debug.LogWarning("클립이 비어있음!");
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -36,96 +36,12 @@ public class SoundManager : MonoBehaviour
     }
     #endregion singleton
 
-    // BGM 출력할 오디오 소스
-    [SerializeField]
-    AudioSource audioBgm;
-    
-    //BGM 목록
-    [Header("음악 목록")]
-    public AudioClip[] gameStartMusic;
-    public AudioClip[] gamePlayMusic;
-    public AudioClip[] gameResultMusic;
-
     // 효과음
     [SerializeField]
     Sound[] sfxs;
     // 효과음 출력할 오디오 소스
     [SerializeField]
     AudioSource[] audioSfx; // 효과음은 중첩되어 들릴 수 있으므로 배열로 설정
-
-    //씬 로드 완료 시
-    void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    //오브젝트 비활성화 시
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    //씬 로드 완료되면 호출
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        ChooseBGM(scene.name);
-    }
-
-    // BGM 재생(씬이 로드될 때마다 호출)
-    void ChooseBGM(string sceneName)
-    {
-        switch (sceneName)
-        {
-            //case "GameStartScene":
-            case "GameStartScene_withMusic":
-                if (gameStartMusic != null)
-                {
-                    if (gameStartMusic.Length >= 2) StartCoroutine(PlayIntroLoopMusic(gameStartMusic[0], gameStartMusic[1]));
-                    else if (gameStartMusic.Length > 0) PlayLoopMusic(gameStartMusic[0]);
-                }
-                break;
-            case "GamePlayScene":
-                if (gameStartMusic != null)
-                {
-                    if (gamePlayMusic != null) PlayLoopMusic(gamePlayMusic[0]);
-                }
-                break;
-            case "GameResultScene":
-                audioBgm.Stop();
-                break;
-        }
-    }
-
-    //시작부분 한번 연주하고, 계속 반복해서 뒷부분 연주
-    IEnumerator PlayIntroLoopMusic(AudioClip introMusic, AudioClip loopMusic)
-    {
-        audioBgm.Stop();
-        //시작부분 한번만 연주
-        audioBgm.clip = introMusic;
-        audioBgm.loop = false;
-        audioBgm.Play();
-
-        //시작부분 음악만큼 기다리기
-        yield return new WaitForSeconds(introMusic.length - 1.2f);
-
-        //반복 연주 부분 계속해서 연주하기
-        audioBgm.clip = loopMusic;
-        audioBgm.loop = true;
-        audioBgm.Play();
-    }
-
-    //시작부분 없이 하나만 반복해서 연주하기
-    void PlayLoopMusic(AudioClip clip)
-    {
-        audioBgm.Stop();
-        if (clip != null)
-        {
-            audioBgm.clip = clip;
-            audioBgm.loop = true;
-            audioBgm.Play();
-        }
-        else Debug.LogWarning("클립이 비어있음!");
-    }
 
     // 재생 중인 효과음 이름
     public string[] playSoundName;
