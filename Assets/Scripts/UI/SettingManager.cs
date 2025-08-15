f0931ca09056b8f696e5eaf02c4af3f9e23f0c8d
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-08-16 07:43:13 +0900

    feat(setting): 설정 기능 구현중
    - SettingManager를 싱글톤으로 구현
    - 싱글톤으로 구현했으므로 기존 컴포넌트인 SettingManager를 새로운 빈 오브젝트인 SettingManager로 옮김
    SettingManager에 있던 기존 딕셔너리를 삭제하고, 그냥 float형 변수 세 개로 바꿈. 이것이 더욱 간단해보이기 때문.
    - 씬의 호출을 이름 방식이 아니라, GameManager에서 이름을 선언한 뒤 그걸 참조해 사용하는 방식으로 바꿈. 테스트중인 씬을 사용하기 위해 매번 이름을 참조하는 것이 많이 번거로웠기 때문.

--- Assets/Scripts/UI/SettingManager.cs
+++ Assets/Scripts/UI/SettingManager.cs
@@ -1,47 +1,150 @@
 using System;
 using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEditor.PackageManager;
 using UnityEditor.Rendering;
 using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SettingManager : MonoBehaviour
+public class SettingManager : Singleton<SettingManager>
 {
     [Header("설정 슬라이더")]
     [SerializeField] Slider sfxSlider;
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider mouseSensSlider;
 
-    //세팅값 저장하기 위한 딕셔너리
-    public Dictionary<SettingEnum, float> SettingValue = new Dictionary<SettingEnum, float>();
-    
-    //게임 세이브데이터 접근 위한 변수
+    [Header("음성 설정을 위한 오브젝트들")]
+    [SerializeField] GameObject sfxManager;
+    [SerializeField] GameObject bgmManager;
+
+    [Header("컴포넌트 참조 변수들")]
+    [SerializeField] PlayerController playerController;
+    [SerializeField] GameManager gameManager;
+
+    string sceneName; //씬 이름 저장하기 위한 변수
+
+    float bgmVolume;
+    float sfxVolume;
+    float mouseSens;
+
+    protected override void Awake()
+    {
+    }
+
+    void Start()
+    {
+        ApplySetting(); //설정값 불러와서 설정 적용
+    }
 
     private void Update()
     {
-        //이후 최적화를 위해 전 값과 비교하고, 전 값과 같으면 할당을 스킵하는 기능을 추가해야 함.
-        SettingToValue();
         ChangeSetting();
     }
 
-    //딕셔너리에 SettingEnum enum 키와 슬라이더 값 value 추가하는 메서드
-    void SettingToValue()
+    private void OnEnable()
     {
-        if (sfxSlider != null)
-            SettingValue[SettingEnum.SFX] = sfxSlider.value;
-        if (bgmSlider != null)
-            SettingValue[SettingEnum.BGM] = bgmSlider.value;
-        if (mouseSensSlider != null)
-            SettingValue[SettingEnum.MOUSE_SENS] = mouseSensSlider.value;
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    //설정 덮어쓰는 메서드
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //씬이 처음 로드되면 
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log($"SettingManager: {scene.name} 씬이 로드되었습니다.");
+        
+        //씬의 이름을 변수에 배정 
+        SetSceneName(scene, sceneName);
+
+        //플레이어 컨트롤러가 인스펙터에서 비어있는 경우
+        if (playerController == null)
+        {
+            FindPlayerController();
+            FindSettings();
+        }
+    }
+
+    //플레이어가 없는 경우, 플레이어를 찾아 배정하는 메서드
+    void FindPlayerController()
+    {
+        if (sceneName != gameManager.nameOfPlayScene)
+        {
+            Debug.LogError("Player가 존재하지 않습니다");
+        }
+        else
+        {
+            //플레이어 찾기
+            GameObject playerObject = GameObject.FindWithTag("Player");
+            playerController = playerObject.GetComponent<PlayerController>();
+        }
+    }
+
+    void FindSettings()
+    {
+        if (sceneName == gameManager.nameOfStartScene)
+        {
+            sfxSlider
+            bgmSlider
+            mouseSens
+        }
+
+        else if (sceneName == gameManager.nameOfPlayScene)
+        {
+            sfxSlider
+            bgmSlider
+            mouseSens
+        }
+
+        else return;
+    }
+
+    //지정한 변수에 씬의 이름을 넣는 메서드
+    string SetSceneName(Scene scene, string sceneName)
+    {
+        sceneName = scene.name;
+        return sceneName;
+    }
+
+    //슬라이더의 설정 반영하는 메서드
+    //설정이 바뀐 경우에만 수정 실행
     void ChangeSetting()
     {
+        if (DefineSettingChange(sfxVolume, sfxSlider.value))
+            sfxVolume = sfxSlider.value;
+        if (DefineSettingChange(bgmVolume, bgmSlider.value))
+            bgmVolume = bgmSlider.value;
+        if (DefineSettingChange(mouseSens, mouseSensSlider.value))
+            mouseSens = mouseSensSlider.value;
+    }
+
+    //설정이 바뀌었다면 true 반환
+    bool DefineSettingChange(float saved, float current)
+    {
+        if (saved != current) return true;
+        else return false;
+    }
+
+    //설정한 내용을 게임 데이터에 저장하는 메서드
+    public void SaveSetting()
+    {
         GameData gameData = GameManager.Instance.gameData;
-        gameData.sfxVolume = SettingValue[SettingEnum.SFX];
-        gameData.bgmVolume = SettingValue[SettingEnum.BGM];
-        gameData.mouseSensitivity = SettingValue[SettingEnum.MOUSE_SENS];
+        gameData.sfxVolume = sfxVolume;
+        gameData.bgmVolume = bgmVolume;
+        gameData.mouseSensitivity = mouseSens;
+    }
+
+    //설정 적용하는 메서드
+    void ApplySetting()
+    {
+        GameData gameData = GameManager.Instance.gameData;
+        sfxVolume = gameData.sfxVolume;
+        bgmVolume = gameData.bgmVolume;
+        mouseSens = gameData.mouseSensitivity;
     }
 }
