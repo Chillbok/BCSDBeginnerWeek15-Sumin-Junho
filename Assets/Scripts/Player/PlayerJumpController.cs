22c81f9e001f06bafd87769d08a94ba84ebfedd7
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-08-19 04:53:02 +0900

    refactor(player controller): 플레이어의 주요 스테이터스를 스크립터블 오브젝트로 구현함
    스크립트의 이름은 PlayerData.cs

---
+++ Assets/Scripts/Player/PlayerJumpController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerJumpController : MonoBehaviour
+{
+    [Tooltip("플레이어가 떨어질 때 적용할 중력")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jumpForce = 5.5f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
